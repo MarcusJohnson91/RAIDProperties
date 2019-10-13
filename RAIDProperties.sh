10a431ba2bfbdea70a476928f9ee04061108adfb
Marcus Johnson <marcusjohnson10151991@gmail.com>
2019-10-13 07:12:25 -0400

    Fleshed out the script a bit more

--- RAIDProperties.sh
+++ RAIDProperties.sh
@@ -6,9 +6,10 @@
 #Usage: first login to the machine you want to get the details from, then run the script with sudo.
 
 GetHardwareRAIDProperties() {
-    #Use LSPCI
+	Manufacturer=$1 # Argument #1
+    	#Use LSPCI
 	# 01:00.0 RAID bus controller: LSI Logic / Symbios Logic MegaRAID SAS-3 3008 [Fury] (rev 02)
-	# HARDWARE RAID can have multiple "ports", an 8i raid card has 2 ports, with up to 4 drives for each port for a total of 8 drives attached to each raid card. 4i is the same but with 1 port, 4 drives per port, 4 drives total.
+	# HARDWARE RAID can have multiple "ports", an 8i raid card has 2 ports, with up to 4 drives for each port for a total of 8 drives attached to each raid card. 4i is the same but with 	1 port, 4 drives per port, 4 drives total.
 	RAIDPath=$()
 	RAIDManufacturer=$(lspci | grep -i 'raid' | awk '{print $5}')
 	RAIDTyoe=$() # 0, 1, etc
@@ -35,9 +36,6 @@ GetHardwareRAIDProperties() {
 
 GetSoftwareRAIDProperties() {
     #Use MDADM
-}
-
-GetRAIDArrayProperties() {
     RAIDPath=$(mdadm --detail -scan | awk '{printf $2}') # Should always be /dev/md0, not sure what'll happen if there's ever more than 1 RAID array.
     NumRAIDDrives=$(mdadm --detail $"RAIDPath" | grep -i 'Raid Devices : ' | awk '{printf $2}')
     RAIDLevel=$(mdadm --detail $"RAIDPath" | grep -i 'Raid Level : ' | awk '{printf $4}') # 0, 1, 10, 4, 5, 6
@@ -57,7 +55,7 @@ GetBasicDriveProperties() {
 
 LSPCIPath=$(which lspci)
 LSBLKPath=$(which lsblk)
-DMADMPath=$(which dmadm)
+MDADMPath=$(which mdadm)
 
 if [ $(tail -n 1 $LSPCIPath) -eq ")" ] # Need to install lspci
 	DNFPath=$(which dnf)
@@ -87,19 +85,19 @@ if [ $(tail -n 1 $LSBLKPath) -eq ")" ] # Need to install LSBLKPath
 	fi
 fi
 
-if [ $(tail -n 1 $DMADMPath) -eq ")" ]
+if [ $(tail -n 1 $MDADMPath) -eq ")" ]
 	DNFPath=$(which dnf)
 	APTPath=$(which apt)
 	YUMPath=$(which yum)
 	if [ $(tail -n 1 $DNFPath) -ne ")" ] # DNF is available
-		dnf install dmadm
+		dnf install mdadm
 	elif [ $(tail -n 1 $DNFPath) -ne ")" ] # APT is available, use it if we need to
 		apt update
-		apt install dmadm
+		apt install mdadm
 	elif [ $(tail -n 1 $YUMPath) -ne ")" ] # Use YUM as a last resort
-		yum install dmadm
+		yum install mdadm
 	fi
-fi
+fiping
 
 # there may also be multiple RAID cards, we need to pul in all of that information.
 # So, the heirarchy of information we need:
@@ -109,9 +107,29 @@ fi
 # 4: RAID card manufacturer, model, hardware revision, and firmware info (revision and firmwre just to be safe/future expansion)
 # Partitioning info for all RAID arrays
  
+NumberOfHardwareRAIDCards=$(lspci -vv | grep -i 'RAID bus controller' | wc -l)
+if [ $NumberOfHardwareRAIDCards -ge 1 ]; then
+	if [ $(lspci -knn | grep 'RAID bus controller' | awk '{print $8}') -eq "LSI" ]; then
+		GetHardwareRAIDProperties LSI
+	elif [ $(lspci -knn | grep 'RAID bus controller' | awk '{print $5}') -eq "Adaptec" ]; then
+		GetHardwareRAIDProperties Adaptec
+	else
+		echo "Unknown RAID card manufacturer!"
+		exit
+	fi
+else
+	if [ $(mdadm --detail -scan | grep -i 'ARRAY' | wc -l) -ge 1 ]; then
+		# Software RAID found
+		GetSoftwareRAIDProperties
+	fi
+	
+fi
+
+
+
 
 NumberOfSoftwareRAIDArrays=$(mdadm --detail -scan | grep -i 'ARRAY' | wc -l)
-NumberOfHardwareRAIDCards=$(lspci -vv | grep -i 'raid' | wc -l)
+NumberOfHardwareRAIDPorts=0
 NumberOfHardwareRAIDArrays=$(lspci -vv | grep -i 'raid')
 NumberOfRAIDArrays=$($NumberOfSoftwareRAIDArrays + $NumberOfHardwareRAIDArrays)
 if [ "$NumberOfSoftwareRAIDArrays" -ge 1 ]; then
@@ -119,8 +137,9 @@ if [ "$NumberOfSoftwareRAIDArrays" -ge 1 ]; then
 elif [ "$NumberOfHardwareRAIDArrays" -ge 1 ]; then
     # Do the RAID specific stuff here
     # Then call the BasicDriveProperties function
-    GetHardwareRAIDProperties
     GetBasicDriveProperties
+    GetHardwareRAIDProperties
+    
 else
     echo "RAID: NOT PRESENT"
     # Call the BasicDriveProperties function
